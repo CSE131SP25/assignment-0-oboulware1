e96b684e49fd5b5c52e2538cadf27eaefff809dd
oniboulware <oniboulware@localhost>
2025-01-21 20:18:49 -0600

    completed robot instructions

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,19 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.forward(150);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(25);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(50);
+		robot.turnRight(90);
+		robot.forward(50);
+		robot.turnRight(90);
+		robot.forward(50);
+		robot.turnRight(90);
+		robot.forward(25);
+		robot.turnLeft(90);
+		robot.forward(150);
 	}
 
 }
